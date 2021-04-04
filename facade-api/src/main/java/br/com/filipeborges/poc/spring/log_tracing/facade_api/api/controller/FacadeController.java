e8eb39044b54fbafd349eb5923b88709270398e5
Filipe Conde <filipe_conde@terceiros.sicredi.com.br>
2021-04-04 18:33:12 -0300

    CustomWebFilter to log incoming request transactionId header

--- facade-api/src/main/java/br/com/filipeborges/poc/spring/log_tracing/facade_api/api/controller/FacadeController.java
+++ facade-api/src/main/java/br/com/filipeborges/poc/spring/log_tracing/facade_api/api/controller/FacadeController.java
@@ -7,24 +7,26 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.util.context.ContextView;
 
+import java.util.Optional;
+
 @RestController
 public class FacadeController {
 
     @GetMapping("/calculate")
     public Mono<Void> calculate() {
         return Mono.just(true)
-                .transformDeferredContextual(this::printTransactionIdRequestHeader)
+                .transformDeferredContextual(this::logRequestContextTransformation)
                 .then();
     }
 
-    // TODO: Move to WebFilter
     // https://projectreactor.io/docs/core/release/reference/#context
-    private <T> Mono<T> printTransactionIdRequestHeader(Mono<T> upstreamMono, ContextView contextView) {
-        return Mono.just((ServerWebExchange)contextView.get(ServerWebExchangeContextFilter.EXCHANGE_CONTEXT_ATTRIBUTE))
-                .map(serverWebExchange -> serverWebExchange.getRequest().getHeaders().get("TransactionID").get(0))
-                .doOnNext(tid -> System.out.println("=========> TID From Header: " + tid))
-                .flatMap(_s -> upstreamMono)
-                .switchIfEmpty(upstreamMono);
+    private <T> Mono<T> logRequestContextTransformation(Mono<T> upstreamMono, ContextView contextView) {
+        return Optional.of((ServerWebExchange)contextView.get(ServerWebExchangeContextFilter.EXCHANGE_CONTEXT_ATTRIBUTE))
+                .map(serverWebExchange -> serverWebExchange.getRequest().getHeaders().get("TransactionID"))
+                .map(values -> values.get(0))
+                .map(tid -> "=========> TransactionID: " + tid)
+                .map(upstreamMono::log)
+                .orElse(upstreamMono);
     }
 
 }
--- facade-api/src/main/java/br/com/filipeborges/poc/spring/log_tracing/facade_api/configuration/ApiConfiguration.java
+++ facade-api/src/main/java/br/com/filipeborges/poc/spring/log_tracing/facade_api/configuration/ApiConfiguration.java
@@ -1,8 +1,16 @@
 package br.com.filipeborges.poc.spring.log_tracing.facade_api.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.reactive.ServerWebExchangeContextFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Configuration
 public class ApiConfiguration {
@@ -11,6 +19,21 @@ public class ApiConfiguration {
     @Bean
     public ServerWebExchangeContextFilter addServerWebExchangeToReactorContext() {
         return new ServerWebExchangeContextFilter();
+    }
+
+    @Bean
+    public WebFilter requestContextLoggingFilter() {
+        final Logger logger = LoggerFactory.getLogger("CustomWebFilter");
+
+        return new WebFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+                Optional.ofNullable(exchange.getRequest().getHeaders().get("TransactionID"))
+                        .map(values -> values.get(0))
+                        .ifPresent(tid -> logger.info("=======> TransactionID: " + tid));
+                return chain.filter(exchange);
+            }
+        };
     }
 
 }
