e14aadd242b9d4df6f62a78f0bbd78d680636612
Filipe Conde <filipe_conde@terceiros.sicredi.com.br>
2021-05-02 12:35:49 -0300

    Added lib auto configuration

--- logging-lib/lib/src/main/java/br/com/filipeborges/poc/spring/log_tracing/log/configuration/CustomLogAutoConfiguration.java
+++ logging-lib/lib/src/main/java/br/com/filipeborges/poc/spring/log_tracing/log/configuration/CustomLogAutoConfiguration.java
@@ -1,12 +1,14 @@
 package br.com.filipeborges.poc.spring.log_tracing.log.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.reactive.ServerWebExchangeContextFilter;
 
 @Configuration
-public class LogConfiguration {
+public class CustomLogAutoConfiguration {
     //https://docs.spring.io/spring-framework/docs/5.3.5/javadoc-api/org/springframework/web/filter/reactive/ServerWebExchangeContextFilter.html
+    @ConditionalOnMissingBean
     @Bean
     public ServerWebExchangeContextFilter addServerWebExchangeToReactorContext() {
         return new ServerWebExchangeContextFilter();
